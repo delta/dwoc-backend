d4f3204add481bc2ddbed60db2eaf912fa0afdf0
Adarsh Patel <adarshpatel509@gmail.com>
2019-09-26 00:39:59 +0530

    Add updateUser resolver with session validation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,9 +1,24 @@
+import { authenticate } from '../authentication';
+
 function createNewUser(root, args, context) {
   return context.prisma.createUser({
     ...args.data,
   });
 }
 
+async function updateUser(root, args, context) {
+  //check if user has a valid session
+  const authRes = await authenticate(context.session, context.id);
+  if (!authRes.isAuth) {
+    throw new Error('login again!');
+  }
+  return context.prisma.updateUser({
+    data: args.data,
+    where: args.where,
+  });
+}
+
 module.exports = {
   createNewUser,
+  updateUser,
 };
