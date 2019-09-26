8bf8525050cce761e328ecfa4751b92d029839b9
PranavShriram <pranavshriram99@gmail.com>
2019-09-26 20:35:52 +0530

    Added query for organization

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -6,6 +6,13 @@ function info(): string {
   return 'Welcome to DWOC';
 }
 
+async function organizations(root, args, context) {
+
+  return context.prisma.organizations({
+    where: args.where,
+  });
+}
 module.exports = {
   info,
+  organizations
 };
