0d0cc3760b08fae4745b777ad12720a6a392ff70
Mario Jones <mariovimal@gmail.com>
2019-09-28 23:04:36 +0530

    Remove Unused types in Schema

--- src/auth.js
+++ src/auth.js
@@ -2,26 +2,26 @@
  * @flow
  */
 
-import passport from 'passport';
-import GitHubStrategy from 'passport-github';
-import { prisma } from './generated/prisma-client';
+import passport from "passport";
+import GitHubStrategy from "passport-github";
+import { prisma } from "./generated/prisma-client";
 
 type User = {
   profile: {
     name: string,
     login: string,
     email: string,
-    [key: string]: any,
+    [key: string]: any
   },
-  accessToken: string,
+  accessToken: string
 };
 
 passport.use(
   new GitHubStrategy(
     {
-      clientID: '0a73f6b3c7a2a72eb0c2',
-      clientSecret: '8ecf6ade70c3768635f21ef6df9a1170b5d28934',
-      callbackURL: 'http://localhost:4000/home',
+      clientID: "0a73f6b3c7a2a72eb0c2",
+      clientSecret: "8ecf6ade70c3768635f21ef6df9a1170b5d28934",
+      callbackURL: "http://localhost:4000/home"
     },
     function(
       accessToken: string,
@@ -37,19 +37,19 @@ passport.use(
 passport.serializeUser(async (user: User, cb: (a?: any, b?: any) => any) => {
   //create a new user if user does not exist
   let findUser = await prisma.users({
-    where: { githubHandle: user.profile.login },
+    where: { githubHandle: user.profile.login }
   });
 
   if (findUser.length == 0 || findUser == null || findUser == undefined) {
-    const name = user.profile.name.split(' ');
+    const name = user.profile.name.split(" ");
     const firstName = name[0];
-    const lastName = name.length > 1 ? name[name.length - 1] : ' ';
+    const lastName = name.length > 1 ? name[name.length - 1] : " ";
     let newUser = await prisma.createUser({
       firstName: firstName,
       lastName: lastName,
       email: user.profile.email,
       githubHandle: user.profile.login,
-      session: user.accessToken,
+      session: user.accessToken
     });
     // console.log('NewUser Created successfullt', newUser);
     cb(null, newUser);
@@ -57,13 +57,13 @@ passport.serializeUser(async (user: User, cb: (a?: any, b?: any) => any) => {
     //if user already exist then update the access token
     let updatedUser = await prisma.updateUser({
       data: { session: user.accessToken },
-      where: { githubHandle: user.profile.login },
+      where: { githubHandle: user.profile.login }
     });
     // console.log('Session update successfully', updatedUser);
     cb(null, updatedUser);
   }
 });
 
-passport.deserializeUser(function (user, cb) {
+passport.deserializeUser(function(user, cb) {
   cb(null, user.profile);
 });
--- src/index.js
+++ src/index.js
@@ -2,27 +2,27 @@
  * @flow
  */
 
-import { GraphQLServer } from 'graphql-yoga';
-import passport from 'passport';
-import { prisma } from './generated/prisma-client';
-import resolvers from './resolvers';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
+import { GraphQLServer } from "graphql-yoga";
+import passport from "passport";
+import { prisma } from "./generated/prisma-client";
+import resolvers from "./resolvers";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
-require('./auth.js');
+require("./auth.js");
 
 type Request = {
   request: {
     headers: {
       session: string,
       id: string,
-      [key: string]: any,
-    },
-  },
+      [key: string]: any
+    }
+  }
 };
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+  typeDefs: "./src/schema.graphql",
   resolvers,
 
   context: (req: Request) => {
@@ -31,9 +31,9 @@ const server = new GraphQLServer({
     return {
       session,
       id,
-      prisma,
+      prisma
     };
-  },
+  }
 });
 
 server.express.use(cookieParser());
@@ -41,26 +41,26 @@ server.express.use(bodyParser.urlencoded({ extended: true }));
 server.express.use(passport.initialize());
 server.express.use(passport.session());
 
-server.express.get('/login', passport.authenticate('github'));
+server.express.get("/login", passport.authenticate("github"));
 server.express.get(
-  '/home',
-  passport.authenticate('github', {
-    failureRedirect: 'http://localhost:4000/login',
+  "/home",
+  passport.authenticate("github", {
+    failureRedirect: "http://localhost:4000/login"
   }),
   function(req, res) {
     let session = req.session.passport.user;
     //send cookie to user browser
     res.cookie(
-      'dwoc_user_session',
+      "dwoc_user_session",
       JSON.stringify({ session: session.session, id: session.id }),
       { encode: String }
     );
-    res.send('Welcome to DWOC');
+    res.send("Welcome to DWOC");
   }
 );
 
-server.express.get('/logout', (req, res, next) => {
-  res.redirect('https://github.com/logout');
+server.express.get("/logout", (req, res, next) => {
+  res.redirect("https://github.com/logout");
 });
 
-server.start(() => console.log('Server is running on http://localhost:4000'));
+server.start(() => console.log("Server is running on http://localhost:4000"));
