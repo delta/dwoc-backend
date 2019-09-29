0fc6c886392b614af6057932fd2e1231522b473b
Adarsh Patel <adarshpatel509@gmail.com>
2019-09-29 14:30:41 +0530

    Add userProfile, userProposals resolvers & fix nested query error

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -21,10 +21,23 @@ function info(): string {
   return "Welcome to DWOC";
 }
 
+async function userProfile(root: any, args: Args, context: Context) {
+  const authRes = await authenticate(context.session, context.id);
+  if(!authRes.isAuth) {
+    throw new Error("Login Again!!");
+  }
+  return await context.prisma.user({
+    id: context.id
+  });
+}
+
 async function users(root: any, args: Args, context: Context) {
   const authRes = await authenticate(context.session, context.id);
   if (!authRes.isAuth) {
     throw new Error("Login Again!!");
+  }
+  if(authRes.role !== "Admin") {
+    throw new Error("Access Denied!!");
   }
   return await context.prisma.users({
     where: args.where
@@ -32,14 +45,24 @@ async function users(root: any, args: Args, context: Context) {
 }
 
 async function projects(root: any, args: Args, context: Context) {
-  return context.prisma.projects({
+  return await context.prisma.projects({
     where: args.where
   });
 }
 
 async function organizations(root: any, args: Args, context: Context) {
-  return context.prisma.organizations({
+  return await context.prisma.organizations({
     where: args.where
+  });
+}
+
+async function userProposals(root: any, args: Args, context: Context) {
+  const authRes = await authenticate(context.session, context.id);
+  if (!authRes.isAuth) {
+    throw new Error("Login Again!!");
+  }
+  return await context.prisma.proposals({
+    where: {user: {id: context.id}}
   });
 }
 
@@ -51,22 +74,25 @@ async function proposals(root: any, args: Args, context: Context) {
   if (authRes.role !== "Mentor" && authRes.role !== "Admin") {
     throw new Error("Access Denied!");
   }
-  return context.prisma.proposals({
+  return await context.prisma.proposals({
     where: args.where
   });
 }
 
 async function mentors(root: any, args: Args, context: Context) {
   const authRes = await authenticate(context.session, context.id);
-  return context.prisma.mentors({
+  return await context.prisma.mentors({
     where: args.where
   });
 }
 
 module.exports = {
   info,
+  userProfile,
+  users,
   projects,
-  proposals,
   organizations,
+  userProposals,
+  proposals,
   mentors
 };
